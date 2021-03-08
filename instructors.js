b61553da088bf431695e1ad46a519d7efd7015da
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-08 12:00:01 -0300

    criando datas no js, e estruturando dados internos

--- instructors.js
+++ instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require('./data.json')
 
 // create
 exports.post = function (req, res) {
@@ -11,7 +12,25 @@ exports.post = function (req, res) {
             return res.send('Preencha todos os campos!')
         }
     }
-    fs.writeFile('data.json', JSON.stringify(req.body), function () {
+
+    let { avatar_url, birth, name, services, gender } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        avatar_url,
+        birth,
+        created_at,
+        id,
+        gender,
+        services,
+        name
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function () {
         if (err) return res.send("Write file error")
 
         return res.redirect("/instructors")
