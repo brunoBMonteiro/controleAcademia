36029926ee56cb5e9883ea94441af15f3e4ea5c9
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-29 21:18:36 -0300

    Lógica do menu ativo

--- public/script.js
+++ public/script.js
@@ -1,24 +1,8 @@
-const modalOverlay = document.querySelector(".modal-overlay");
-const modal = document.querySelector(".modal")
-const cards = document.querySelectorAll(".card");
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
 
-for(let card of cards) {
-    card.addEventListener("click", function(){
-        const blogId = card.getAttribute("id");
-        modalOverlay.classList.add("active");
-        modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${blogId}`
-    });
-}
-
-document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove("active")
-    modalOverlay.querySelector("iframe").src = ""
-})
-
-document.querySelector(".maximize-modal").addEventListener("click", function(){
-    if (modal.classList.contains("maximized")) {
-        modal.classList.remove("maximized");
-    } else {
-        modal.classList.add("maximized");
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
     }
-});
+}
